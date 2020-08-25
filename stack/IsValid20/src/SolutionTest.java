f447d6eebca0326a94ac8010ab3acd6f91e356e3
black_wang <rainnns@users.noreply.github.com>
2020-08-25 20:25:15 +0800

    时隔俩个月的记录分支突然不知道怎么就不见了，我哭了

---
+++ stack/IsValid20/src/SolutionTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author wwx-sys
+ * @time 2020-08-25-20:23
+ * @description
+ */
+class SolutionTest {
+
+    @Test
+    void isValid() {
+        Solution solution = new Solution();
+        System.out.println(solution.isValid(""));
+    }
+}
