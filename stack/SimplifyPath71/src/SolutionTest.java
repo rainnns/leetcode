0da18189935f1704d5735e8bd8ea5e6bfb0c036c
black_wang <rainnns@users.noreply.github.com>
2020-08-28 16:04:36 +0800

    .71 简化路径

---
+++ stack/SimplifyPath71/src/SolutionTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author wwx-sys
+ * @time 2020-08-28-14:29
+ * @description
+ */
+class SolutionTest {
+
+    @Test
+    void simplifyPath() {
+        Solution solution = new Solution();
+        System.out.println(solution.simplifyPath("/../"));
+    }
+}
