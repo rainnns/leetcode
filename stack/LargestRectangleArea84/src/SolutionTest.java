310ca1e56ad5f90e3c9bcecd496b464611ecf74b
black_wang <rainnns@users.noreply.github.com>
2020-09-04 21:16:30 +0800

    .84 柱状图中最大的矩形
    单调栈 + 哨兵

---
+++ stack/LargestRectangleArea84/src/SolutionTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author wwx-sys
+ * @time 2020-09-04-20:09
+ * @description
+ */
+class SolutionTest {
+
+    @Test
+    void largestRectangleArea() {
+        Solution solution = new Solution();
+        System.out.println(solution.largestRectangleArea(new int[]{2,1,5,6,2,3}));
+    }
+}
