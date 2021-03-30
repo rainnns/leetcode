e55dae1393f0aaee021fdd6f73217ee7ad558ba5
black_wang <1750546526@qq.com>
2021-03-30 21:27:38 +0800

    经典回溯算法
    通过三个集合，将不能同列、斜率不能为1 和 -1 转化为 x+y 不相同和 x-y 不相同

---
+++ backTracking/solveNQueens0812/src/SolutionTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author wwx-sys
+ * @time 2021-03-12-15:27
+ * @description
+ */
+class SolutionTest {
+
+    @Test
+    void solveNQueens() {
+        System.out.println(new Solution().solveNQueens(4));
+//        int[] cols = new int[]{2,4,1,3};
+//        System.out.println(new Solution().is_inline(cols,4));
+    }
+}
