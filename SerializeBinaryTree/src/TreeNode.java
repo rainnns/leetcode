1cf9a46644bd70a3d808c2e4fb524651a1873898
wangwenxuan <46552185+rainnns@users.noreply.github.com>
2021-06-30 18:26:11 +0800

    序列化与反序列化二叉树

---
+++ SerializeBinaryTree/src/TreeNode.java
@@ -0,0 +1,14 @@
+/**
+ * @description:
+ * @author: black-wang
+ * @createDate: 2021/6/30 18:24
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
