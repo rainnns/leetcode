6d0a0b2d149113c70d99c03e875c1b2e2d05658a
wwx <1750546526@qq.com>
2021-07-21 15:29:12 +0800

    链表题  哈希集合  / 双指针
    浪漫双指针：
    你变成我，走过我走过的路。
    我变成你，走过你走过的路。
    然后我们便相遇了..

---
+++ GetIntersectionNode剑指 Offer 52. 两个链表的第一个公共节点/src/Solution.java
@@ -0,0 +1,64 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author wwx-sys
+ * @time 2021-07-21-14:57
+ * @description
+ */
+
+
+public class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    /**
+     * 每步操作需要同时更新指针 pA 和 pB。
+     *
+     * 如果指针 pA 不为空，则将指针 pA 移到下一个节点；如果指针 pB 不为空，则将指针 pB 移到下一个节点。
+     *
+     * 如果指针 pA 为空，则将指针 pA 移到链表 headB 的头节点；如果指针 pB 为空，则将指针 pB 移到链表 headA 的头节点。
+     *
+     * 当指针 pA 和 pB 指向同一个节点或者都为空时，返回它们指向的节点或者 null。
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+
+    //哈希集合
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        Set<ListNode> visited = new HashSet<ListNode>();
+        ListNode temp = headA;
+        while (temp != null) {
+            visited.add(temp);
+            temp = temp.next;
+        }
+        temp = headB;
+        while (temp != null) {
+            if (visited.contains(temp)) {
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+
+}
