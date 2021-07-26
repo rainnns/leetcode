4a12f8e44befbaa5a44fd14ea18ae94cd680cd69
wwx <1750546526@qq.com>
2021-07-26 18:40:20 +0800

    得到子序列的最少操作次数
    最长公共子序列问题（LCS）可以转换为最长上升子序列问题（LIS）进行求解
    贪心 + 二分

---
+++ MinOperations1713. 得到子序列的最少操作次数/src/Solution.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author wwx-sys
+ * @time 2021-07-26-15:19
+ * @description
+ */
+public class Solution {
+
+    //本题的数据范围为 10^5，使用朴素求解 最长公共子序列问题 LCS 的做法必然超时
+    //当其中一个数组元素各不相同时，最长公共子序列问题（LCS）可以转换为最长上升子序列问题（LIS）进行求解。同时最长上升子序列问题（LIS）存在使用「维护单调序列 + 二分」的贪心解法，复杂度为 O(nlogn)。
+    //动态规划 + 贪心 + 二分
+    public int minOperations(int[] target, int[] arr) {
+        int n = target.length, m = arr.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(target[i], i);
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int x : arr) {
+            if (map.containsKey(x)) list.add(map.get(x));
+        }
+        int len = list.size();
+        int[] f = new int[len], g = new int[len + 1];
+        Arrays.fill(g, Integer.MAX_VALUE);
+        int max = 0;
+        for (int i = 0; i < len; i++) {
+            int l = 0, r = len;
+            while (l < r) {
+                int mid = l + r + 1 >> 1;
+                if (g[mid] < list.get(i)) l = mid;
+                else r = mid - 1;
+            }
+            int clen = r + 1;
+            f[i] = clen;
+            g[clen] = Math.min(g[clen], list.get(i));
+            max = Math.max(max, clen);
+        }
+        return n - max;
+
+    }
+
+
+     public int minOperations1(int[] target, int[] arr) {
+        int n = target.length;
+        Map<Integer, Integer> pos = new HashMap<Integer, Integer>();
+        for (int i = 0; i < n; ++i) {
+            pos.put(target[i], i);
+        }
+        List<Integer> d = new ArrayList<Integer>();
+        for (int val : arr) {
+            if (pos.containsKey(val)) {
+                int idx = pos.get(val);
+                int it = binarySearch(d, idx);
+                if (it != d.size()) {
+                    d.set(it, idx);
+                } else {
+                    d.add(idx);
+                }
+            }
+        }
+        return n - d.size();
+    }
+
+    public int binarySearch(List<Integer> d, int target) {
+        int size = d.size();
+        if (size == 0 || d.get(size - 1) < target) {
+            return size;
+        }
+        int low = 0, high = size - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (d.get(mid) < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+}
