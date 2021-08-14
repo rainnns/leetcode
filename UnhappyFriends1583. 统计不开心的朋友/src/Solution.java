c44d73c8700aa1635f55369998f0c6a14de03292
wangwenxuan <46552185+rainnns@users.noreply.github.com>
2021-08-14 17:46:48 +0800

    统计不开心的朋友
    模拟题

---
+++ UnhappyFriends1583. 统计不开心的朋友/src/Solution.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description:
+ * @author: black-wang
+ * @createDate: 2021/8/14 17:00
+ */
+public class Solution {
+    public int unhappyFriends(int n, int[][] preferences, int[][] pairs) {
+        int[][] order = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                order[i][preferences[i][j]] = j;
+            }
+        }
+        int[] match = new int[n];
+        for (int[] pair : pairs) {
+            int person0 = pair[0], person1 = pair[1];
+            match[person0] = person1;
+            match[person1] = person0;
+        }
+        int unhappyCount = 0;
+        for (int x = 0; x < n; x++) {
+            int y = match[x];
+            int index = order[x][y];
+            for (int i = 0; i < index; i++) {
+                int u = preferences[x][i];
+                int v = match[u];
+                if (order[u][x] < order[u][v]) {
+                    unhappyCount++;
+                    break;
+                }
+            }
+        }
+        return unhappyCount;
+
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        int[][] preferences = new int[][]{{1, 2, 3}, {3, 2, 0}, {3, 1, 0}, {1, 2, 0}};
+        int[][] pairs = new int[][]{{0, 1}, {2, 3}};
+        System.out.println(new Solution().unhappyFriends(n, preferences, pairs));
+    }
+}
