0f2dce8bf96a3b4751128696c7a042443cd15241
wangwenxuan <46552185+rainnns@users.noreply.github.com>
2021-08-24 11:55:25 +0800

    K 站中转内最便宜的航班

---
+++ FindCheapestPrice787. K 站中转内最便宜的航班/src/Solution.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+/**
+ * @description:
+ * @author: black-wang
+ * @createDate: 2021/8/24 11:36
+ */
+public class Solution {
+    final int INF = 10000 * 101 + 1;
+
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int[][] f = new int[k + 2][n];
+        for (int i = 0; i < k + 2; ++i) {
+            Arrays.fill(f[i], INF);
+        }
+        f[0][src] = 0;
+        for (int t = 1; t <= k + 1; ++t) {
+            for (int[] flight : flights) {
+                int j = flight[0], i = flight[1], cost = flight[2];
+                f[t][i] = Math.min(f[t][i], f[t - 1][j] + cost);
+            }
+        }
+        int ans = INF;
+        for (int t = 1; t <= k + 1; ++t) {
+            ans = Math.min(ans, f[t][dst]);
+        }
+        return ans == INF ? -1 : ans;
+    }
+
+    //下面的代码使用两个一维数组进行状态转移。
+    public int findCheapestPrice1(int n, int[][] flights, int src, int dst, int k) {
+        int[] f = new int[n];
+        Arrays.fill(f, INF);
+        f[src] = 0;
+        int ans = INF;
+        for (int t = 1; t <= k + 1; ++t) {
+            int[] g = new int[n];
+            Arrays.fill(g, INF);
+            for (int[] flight : flights) {
+                int j = flight[0], i = flight[1], cost = flight[2];
+                g[i] = Math.min(g[i], f[j] + cost);
+            }
+            f = g;
+            ans = Math.min(ans, f[dst]);
+        }
+        return ans == INF ? -1 : ans;
+    }
+}
