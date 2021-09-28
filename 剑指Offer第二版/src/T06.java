59f3b63311916f619a8720a7406d11ec3039577d
wwx <1750546526@qq.com>
2021-09-28 11:58:53 +0800

    Update T06.java
    两次遍历

--- 剑指Offer第二版/src/T06.java
+++ 剑指Offer第二版/src/T06.java
@@ -13,7 +13,7 @@ public class T06 {
     public int[] reversePrint(ListNode head) {
         ListNode cur = head;
         LinkedList<Integer> stack = new LinkedList<>();
-        while (cur!= null){
+        while (cur != null) {
             stack.push(cur.val);
             cur = cur.next;
         }
@@ -25,6 +25,7 @@ public class T06 {
     }
 
     LinkedList<Integer> list = new LinkedList<>();
+
     //递归
     public int[] reversePrint1(ListNode head) {
         recur(head);
@@ -35,11 +36,29 @@ public class T06 {
         return res;
     }
 
-    void recur(ListNode node){
-        if (node == null){
+    void recur(ListNode node) {
+        if (node == null) {
             return;
         }
         recur(node.next);
         list.add(node.val);
     }
+
+    //两次遍历
+    public int[] reversePrint2(ListNode head) {
+        ListNode cur = head;
+        int n = 0;
+        while (cur != null) {
+            n++;
+            cur = cur.next;
+        }
+        int[] res = new int[n];
+        cur = head;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] = cur.val;
+            cur = cur.next;
+        }
+        return res;
+    }
+
 }
