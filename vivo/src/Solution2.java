ef38b6ff9e28befd5474279c2c64492440f66bec
wangwenxuan <46552185+rainnns@users.noreply.github.com>
2021-09-28 16:01:12 +0800

    完全数

--- vivo/src/Solution2.java
+++ vivo/src/Solution2.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @description:
  * @author: black-wang
@@ -7,22 +10,21 @@ public class Solution2 {
     /**
      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
      *
-     *
      * @param n int整型 未来空闲工作日
      * @return int整型
      */
-    public int workSchedule (int n) {
+    public int workSchedule(int n) {
         // write code here
-        if (n == 0){
+        if (n == 0) {
             return 0;
         }
-        if (n == 1){
+        if (n == 1) {
             return 1;
         }
-        if (n == 2){
+        if (n == 2) {
             return 2;
         }
-        if (n == 3){
+        if (n == 3) {
             return 4;
         }
         int[] dp = new int[n + 1];
@@ -35,6 +37,39 @@ public class Solution2 {
         return dp[n];
 
 
+    }
 
+
+    public int fn(int a1, int n, int d) {
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            sum += a1 + i * (d + i % 3);
+        }
+        return sum;
+    }
+
+    //完全数
+    public void perfectNum() {
+        for (int i = 2; i <= 33; i++) {
+            long t = (1L << i) - 1;
+            if (isZhiShu(t)) {
+                long perfectNum = (1L << (i - 1)) * t;
+                System.out.println(perfectNum);
+            }
+        }
+    }
+
+    public boolean isZhiShu(long n) {
+        for (int i = 2; i <= Math.sqrt(n); i++)
+        {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        new Solution2().perfectNum();
     }
 }
