87781502541052dda2b759529087c081b0a88448
wwx <1750546526@qq.com>
2021-09-28 16:00:31 +0800

    剑指 Offer 07. 重建二叉树

---
+++ 剑指Offer第二版/src/T07.java
@@ -0,0 +1,16 @@
+import entity.TreeNode;
+
+/**
+ * @author wwx-sys
+ * @time 2021-09-28-11:59
+ * @description 剑指 Offer 07. 重建二叉树
+ */
+public class T07 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        TreeNode root = new TreeNode(preorder[0]);
+
+
+        return null;
+    }
+}
---
+++ 剑指Offer第二版/src/entity/TreeNode.java
@@ -0,0 +1,13 @@
+package entity;
+
+/**
+ * @author wwx-sys
+ * @time 2021-09-28-11:59
+ * @description
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x){val = x;}
+}
