f8bae445ecaf699e0fdbc1bca762ea98ca113eb1
wwx <1750546526@qq.com>
2021-10-05 12:06:19 +0800

    二维动态规划；分p[j-1]是否为’*‘进行判断

--- 剑指Offer第二版/src/T19.java
+++ 剑指Offer第二版/src/T19.java
@@ -4,22 +4,38 @@
  * @description 剑指 Offer 19. 正则表达式匹配
  */
 public class T19 {
+
+    //本题的状态共有 m×n 种，应定义状态矩阵 dp ，dp[i][j] 代表 s[:i] 与 p[:j] 是否可以匹配
     public boolean isMatch(String s, String p) {
-        int i = 0, j = 0;
-        while (i < s.length() && j < p.length()) {
-            char a = s.charAt(i);
-            char b = p.charAt(j);
-            if (a == b) {
-                i++;
-                j++;
-                continue;
-            }
-            if (a == '_' || b == '_') {
-                i = a == '_' ? i + 1 : i;
-                j = b == '_' ? j + 1 : j;
+        int m = s.length();
+        int n = p.length();
+
+        boolean[][] f = new boolean[m + 1][n + 1];
+        f[0][0] = true;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(j - 1) == '*') {
+                    f[i][j] = f[i][j - 2];
+                    if (matches(s, p, i, j - 1)) {
+                        f[i][j] = f[i][j] || f[i - 1][j];
+                    }
+                } else {
+                    if (matches(s, p, i, j)) {
+                        f[i][j] = f[i - 1][j - 1];
+                    }
+                }
             }
         }
+        return f[m][n];
+    }
 
-        return true;
+    public boolean matches(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if (p.charAt(j - 1) == '.') {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
     }
 }
