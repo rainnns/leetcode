dfae0df7832342adeb596b6f604e0353a23a415e
wwx <1750546526@qq.com>
2021-10-20 20:35:24 +0800

    kmp

--- OPPO/src/KMP.java
+++ OPPO/src/KMP.java
@@ -1,15 +1,13 @@
-import java.util.Arrays;
-
 /**
  * @author wwx-sys
  * @time 2021-10-20-16:33
  * @description kmp字符串匹配算法
  */
-public class kmp {
+public class KMP {
     /**
      * @param ts 主串
      * @param ps 模式串
-     * @return
+     * @return 返回匹配的索引
      */
     public int kmp(String ts, String ps) {
         char[] t = ts.toCharArray();
@@ -33,6 +31,36 @@ public class kmp {
         return -1;
     }
 
+    /**
+     * @param ts 主串
+     * @param ps 模式串
+     * @return 返回匹配的个数
+     */
+    public int kmpCount(String ts, String ps) {
+        char[] t = ts.toCharArray();
+        char[] p = ps.toCharArray();
+        //主串的当前游标
+        int i = 0;
+        //模式串的当前游标
+        int j = 0;
+        int[] next = getNext(p);
+        int count = 0;
+        while (i < t.length) {
+            if (j == -1 || t[i] == p[j]) {
+                i++;
+                j++;
+            } else {
+                j = next[j];
+            }
+            if (j == p.length) {
+//                System.out.println(i-j);
+                count++;
+                j = -1;
+            }
+        }
+        return count;
+    }
+
     public int[] getNext(char[] p) {
         int[] next = new int[p.length];
         next[0] = -1;
@@ -43,7 +71,7 @@ public class kmp {
                 if (p[++j] == p[++k]) { // 当两个字符相等时要跳过
                     next[j] = next[k];
                 } else {
-                    next[j] = k;
+                    next[j] = k; //等于最大公共前后缀长度 + 1
                 }
             } else {
                 k = next[k];
@@ -54,6 +82,6 @@ public class kmp {
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(new kmp().getNext("oppo".toCharArray())));
+        System.out.println(new KMP().kmpCount("qweqwoppoadadoppoad", "oppo"));
     }
 }
