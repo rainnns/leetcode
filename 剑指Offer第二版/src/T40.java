a053253fc56e7794b9eab097d05f538ed1cbc229
wwx <1750546526@qq.com>
2021-10-23 12:10:18 +0800

    另一种快排

--- 剑指Offer第二版/src/T40.java
+++ 剑指Offer第二版/src/T40.java
@@ -12,7 +12,7 @@ public class T40 {
         if (k >= arr.length) {
             return arr;
         }
-        return quickSort(arr, 0, arr.length - 1, k);
+        return quickSort1(arr, 0, arr.length - 1, k);
     }
 
     private int[] quickSort(int[] arr, int l, int r, int k) {
@@ -34,6 +34,20 @@ public class T40 {
         return Arrays.copyOf(arr, k);
     }
 
+    private int[] quickSort1(int[] arr, int l, int r, int k) {
+        int i = l, j = r;
+        while (i < j) {
+            while (i < j && arr[j] >= arr[l]) j--;
+            while (i < j && arr[i] <= arr[l]) i++;
+            swap(arr, i, j);
+        }
+        swap(arr, i, l);
+        if (i > k) return quickSort(arr, l, i - 1, k);
+        if (i < k) return quickSort(arr, i + 1, r, k);
+        return Arrays.copyOf(arr, k);
+    }
+
+
     private void swap(int[] arr, int l, int r) {
         int t = arr[l];
         arr[l] = arr[r];
@@ -41,7 +55,7 @@ public class T40 {
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[]{6,21,3,4,9,7,2,10,5};
+        int[] arr = new int[]{6, 21, 3, 4, 9, 7, 2, 10, 5};
         System.out.println(Arrays.toString(new T40().getLeastNumbers(arr, 3)));
     }
 }
